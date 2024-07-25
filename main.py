b151ab7194ab52d9863fec7d22fbc73bb8145afe
Андрей Шеремет <seheremet.and@yandex.ru>
2024-07-25 16:50:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# Произвольное число параметров 3.4
+def single_root_words(root_word, *other_words):
+    same_words = []
+    lower_others = [item.lower() for item in other_words]
+    lower_root = root_word.lower()
+    for item in lower_others:
+        if lower_root in item or item in lower_root:
+            same_words.append(item)
+    return same_words
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
