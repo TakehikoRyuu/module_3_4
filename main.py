03222bf1e4580c7327939d19bf26641492931544
Андрей Шеремет <seheremet.and@yandex.ru>
2024-07-26 16:20:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ def single_root_words(root_word, *other_words):
     same_words = []
     lower_others = [item.lower() for item in other_words]
     lower_root = root_word.lower()
-    for item in lower_others:
-        if lower_root in item or item in lower_root:
-            same_words.append(item)
+    for original_item, lower_item in zip(other_words, lower_others):
+        if lower_root in lower_item or lower_item in lower_root:
+            same_words.append(original_item)
     return same_words
 
 
